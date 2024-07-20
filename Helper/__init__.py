c68c15a082834fbfe1e914b55cdebb62e8073e05
Faizur2ndacc <138018309+Faizur2ndacc@users.noreply.github.com>
2024-07-20 12:37:10 +0530

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,6 +1,6 @@
-__title__ = 'Nexus Gen Bot'
-__author__ = 'Vatos'
-__copyright__ = 'discord.gg/nexustools'
+__title__ = 'Faizur'
+__author__ = 'Faizur'
+__copyright__ = 'Faizur'
 __version__ = '1.0.0'
 
 from Helper.utils import *
